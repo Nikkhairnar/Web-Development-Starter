786362b47b65c1cc67d38d894df1f7fcac2429a2
Nikhil Khairnar <nhkhairnar11122004@gmail.com>
2026-06-23 22:23:44 +0530

    JS

--- JS/02/script.js
+++ JS/02/script.js
@@ -1,4 +1,15 @@
 alert("Hello World")
 
 console.log("Code is running")
-console.log("Code is also running")
+console.log("This Code is also running")
+
+var a = prompt("Enter your number: ")
+console.log("Your number is " + a)
+
+var isTrue = confirm("are you sure want to leave this page")
+if(isTrue){
+    console.log("youre leaving")
+}
+else{
+    console.log("youre not leaving")
+}
