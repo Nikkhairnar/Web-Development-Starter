d7c861eaf4421332425677030876e65cf40cbd19
Nikhil Khairnar <nhkhairnar11122004@gmail.com>
2026-06-24 18:29:01 +0530

    JS

--- JS/03_Variables,Datatypes,Objects/script.js
+++ JS/03_Variables,Datatypes,Objects/script.js
@@ -16,3 +16,23 @@ console.log(typeof a,typeof b,typeof c);
 }
 console.log(a);
 
+let x = "Nik";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+// these are primitive datatypes and typeof null is always object..
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o = {
+    "name":"Nikk",
+    "is_handsome" : true,
+    "job code" : 7600
+    
+}
+console.log(o) 
+o.salary = "100crores";
+console.log(o)
+o.salary = "500crores";
+console.log(o)
