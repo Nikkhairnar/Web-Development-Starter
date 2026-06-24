c12f8de6cad9a2d8ec7431bbce2804cb53445a16
Nikhil Khairnar <nhkhairnar11122004@gmail.com>
2026-06-24 18:19:07 +0530

    JS

--- JS/03_Variables,Datatypes,Objects/script.js
+++ JS/03_Variables,Datatypes,Objects/script.js
@@ -1,10 +1,18 @@
 console.log("HEY")
 
-var a = 5;
-var b = 6;
-var c = "Nikk"
+let a = 5;
+a = a + 1;
+let b = 6;
+let c = "Nikk";
 
-console.log(a+b+8)
-console.log(c)
+console.log(a+b+8);
+console.log(c);
 
-console.log(typeof a,typeof b,typeof c)
+console.log(typeof a,typeof b,typeof c);
+{
+    let a = 66;
+    console.log(a);
+    //here let is seperate
+}
+console.log(a);
+
