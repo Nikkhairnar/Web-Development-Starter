fa5a1299fed1982eff3e30c81978a273abb5f8ce
Nikhil Khairnar <nhkhairnar11122004@gmail.com>
2026-06-25 12:38:47 +0530

    JS

---
+++ JS/08_JS_Arrays/script.js
@@ -0,0 +1,30 @@
+let arr = [1,2,4,5,7]
+
+arr[0] = 5666;
+console.log(arr,typeof arr)
+console.log(arr.length)
+console.log(arr[0])
+
+console.log(arr.toString())
+console.log(arr.join(" and "))
+console.log(arr.pop())
+console.log(arr)
+
+console.log(arr.push(100))
+console.log(arr.push("Nikk"))
+console.log(arr)
+
+console.log(arr.shift())
+console.log(arr)
+
+console.log(arr.unshift("Nikhil"))
+console.log(arr)
+
+console.log(delete arr[4])
+console.log(arr)
+
+//.concat is also method 
+
+
+
+
