838253e744cb7934662dc7b304df3e76c37942da
Nikhil Khairnar <nhkhairnar11122004@gmail.com>
2026-06-27 21:39:19 +0530

    JS

---
+++ JS/11_JS_Selecting_by_Ids,Classes/script.js
@@ -0,0 +1,7 @@
+console.log("Nikhil")
+
+let boxes = document.getElementsByClassName("box")
+console.log(boxes)
+//can see at console tab in dev tools chrome
+
+boxes[2].style.backgroundColor = "red"
