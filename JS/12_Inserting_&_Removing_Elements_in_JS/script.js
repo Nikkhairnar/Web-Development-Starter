2e7727b486898c3b8b4cb7e1f46126112a6a8314
Nikhil Khairnar <nhkhairnar11122004@gmail.com>
2026-06-28 12:28:45 +0530

    JS

---
+++ JS/12_Inserting_&_Removing_Elements_in_JS/script.js
@@ -0,0 +1,21 @@
+//try on chrome console tab
+document.querySelector(".box")
+document.querySelector(".box").innerHTML
+document.querySelector(".container").innerHTML
+document.querySelector(".box").innerText
+document.querySelector(".container").innerText
+document.querySelector(".container").outerHTML
+document.querySelector(".container").tagName
+document.querySelector(".container").nodeName
+document.querySelector(".container").textContent
+document.querySelector(".container").hidden
+document.querySelector(".container").hidden = true
+document.querySelector(".box").innerHTML = "Hey i am nikk"
+document.querySelector(".box").hasAttribute("style")
+document.querySelector(".box").getAttribute("style")
+document.querySelector(".box").setAttribute("style","display: inline")
+document.querySelector(".box").attributes
+document.querySelector(".box").removeAttribute("style")
+
+document.designMode = "on" //we can change entire webpage temporarily by using this method in console tab chrome
+document.querySelector(".box").dataset
