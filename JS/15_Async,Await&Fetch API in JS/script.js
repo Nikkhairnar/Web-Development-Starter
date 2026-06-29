b0e95a553aa6cf5e1fd179b20c0b22d9cb26a416
Nikhil Khairnar <nhkhairnar11122004@gmail.com>
2026-06-29 16:19:26 +0530

    JS

---
+++ JS/15_Async,Await&Fetch API in JS/script.js
@@ -0,0 +1,39 @@
+//async function getData() {
+//    //getting data from server
+//    return new Promise((resolve, reject) => {
+//        setTimeout(() => {
+//            resolve(455)
+//        }, 3500);
+//    })
+
+//}
+
+async function getData() {
+    let x = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    let data =  await x.json()
+    console.log(data)
+    return data
+
+}
+
+
+
+async function main() {
+ 
+console.log("Loading modules")
+console.log("Do something else")
+console.log("Load data")
+
+let data = await getData()
+
+console.log(data)
+console.log("Process data")
+console.log("task 2")
+}
+main()
+
+//data.then((v) => {
+//    console.log(data)
+//    console.log("Process data")
+//    console.log("task 2")
+//})
