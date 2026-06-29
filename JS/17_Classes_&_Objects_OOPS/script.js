88aca9207b81ae8d8aaf7f7b6851eb715295a7c3
Nikhil Khairnar <nhkhairnar11122004@gmail.com>
2026-06-29 20:08:11 +0530

    JS

--- JS/17_Classes_&_Objects_OOPS/script.js
+++ JS/17_Classes_&_Objects_OOPS/script.js
@@ -1,5 +1,29 @@
-let obj = {
-    a : 1,
-    b : "Nik"
+class Animal{
+    constructor(name){
+        this.name = name
+        console.log("Object is created....")
+    }
+    eats(){
+        console.log("Kha rha hoon!")
+    }
+    jumps(){
+        console.log("Kood rha hoon!")
+    }
 }
-console.log(obj)
+
+class Lion extends Animal{
+    constructor(name){
+        super(name)
+        console.log("Object is created and it is lion")
+    }
+    eats(){
+        super.eats()
+        console.log("Sher hoon Kha rha hoon!")
+    }
+}
+
+let a = new Animal("Tommy");
+console.log(a)
+
+let l = new Lion("Sheru")
+console.log(l)
